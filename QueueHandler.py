50a6fffebefdf00953c4a66245e320b7ca6bfbbc
sbhat <sbhat@fnal.gov>
2017-11-07 11:49:20 -0600

    Added custom QueueHandler module to repo

---
+++ QueueHandler.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import logging
+import logging.handlers
+
+class QueueHandler(logging.Handler):
+    """
+    This is a logging handler which sends events to a multiprocessing queue.
+    
+    The plan is to add it to Python 3.2, but this can be copy pasted into
+    user code for use with earlier Python versions.
+    """
+
+    def __init__(self, queue):
+        """
+        Initialise an instance, using the passed queue.
+        """
+        logging.Handler.__init__(self)
+        self.queue = queue
+
+    def emit(self, record):
+        """
+        Emit a record.
+
+        Writes the LogRecord to the queue.
+        """
+        try:
+            ei = record.exc_info
+            if ei:
+                # just to get traceback text into record.exc_text
+                dummy = self.format(record)
+                record.exc_info = None  # not needed any more
+            self.queue.put_nowait(record)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+        except:
+            self.handleError(record)
