97d04c80dc55e444c692b197f705f74ac82ffd04
sbhat <sbhat@fnal.gov>
2017-12-11 16:31:02 -0600

    Corrected test error

--- proxy_push.go
+++ proxy_push.go
@@ -266,7 +266,8 @@ func copyProxies(exptConfig *viper.Viper) <-chan copyProxiesStatus {
 func sendExperimentEmail(ename, logfilepath string, recipients []string) error {
 	data, err := ioutil.ReadFile(logfilepath)
 	if err != nil || ename == "darkside" {
-		return err
+		// return err
+		return errors.New("Couldn't read file to send email")
 	}
 
 	msg := string(data)
