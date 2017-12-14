b1b8b194173d53f3a255dd165c2b4db06063c2e6
sbhat <sbhat@fnal.gov>
2017-12-14 16:49:30 -0600

    Printing JSON to debug

--- proxy_push.go
+++ proxy_push.go
@@ -606,6 +606,7 @@ func sendEmail(exptName, message string) error {
 }
 
 func sendSlackMessage(message string) error {
+	fmt.Println(fmt.Sprintf(`{"text": "%s"}`, message))
 	msg := []byte(fmt.Sprintf(`{"text": "%s"}`, message))
 	req, err := http.NewRequest("POST", viper.GetString("notifications.slack_alerts_url"), bytes.NewBuffer(msg))
 	req.Header.Set("Content-Type", "application/json")
