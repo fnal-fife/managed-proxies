b9d370a51b377b51c25b9b5bc1a219f27120c299
sbhat <sbhat@fnal.gov>
2018-02-22 16:07:57 -0600

    Moved proxy transfer tests over to table-based test

--- experimentutil/proxytransfer_test.go
+++ experimentutil/proxytransfer_test.go
@@ -2,6 +2,7 @@ package experimentutil
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 	"testing"
@@ -14,119 +15,64 @@ const badNodeMsgf string = "Node %s didn't respond to pings earlier - " +
 	"ServiceNow ticket that the Scientific Server Infrastructure " +
 	"group reboot the node."
 
-// proxyTransferInfo contains the information needed to describe where on the source host to find the proxy, and where on the destination host to push that proxy
-type goodProxyTransferInfo struct {
-	account           string
-	role              string
-	node              string
-	nodeDown          bool
-	proxyFileName     string
-	proxyFilePathSrc  string
-	proxyFileNameDest string
-}
-
-func (g *goodProxyTransferInfo) createCopyProxiesStatus() copyProxiesStatus {
-	return copyProxiesStatus{g.node, g.account, g.role, nil}
-}
-
-func (g *goodProxyTransferInfo) copyProxy(context.Context, []string) error {
-	return nil
-}
-
-func (g *goodProxyTransferInfo) chmodProxy(context.Context, []string) error {
-	return nil
-}
-
-type badProxyTransferInfo struct {
-	account           string
-	role              string
-	node              string
-	nodeDown          bool
-	proxyFileName     string
-	proxyFilePathSrc  string
-	proxyFileNameDest string
-}
-
-func (b *badProxyTransferInfo) createCopyProxiesStatus() copyProxiesStatus {
-	return copyProxiesStatus{b.node, b.account, b.role, nil}
-}
-
-func (b *badProxyTransferInfo) copyProxy(ctx context.Context, opts []string) error {
-	time.Sleep(1 * time.Second)
-	if e := ctx.Err(); e != nil {
-		return e
-	}
-	return fmt.Errorf("Copying proxy %s to node %s failed.  The error was test error. %s",
-		b.proxyFileName, b.node, b.generateBadNodeMsg())
-}
-
-func (b *badProxyTransferInfo) chmodProxy(ctx context.Context, opts []string) error {
-	time.Sleep(1 * time.Second)
-	if e := ctx.Err(); e != nil {
-		return e
-	}
-	return fmt.Errorf("Error changing permission of proxy %s to mode 400 on %s."+
-		"The error was test error.  %s", b.proxyFileName, b.node, b.generateBadNodeMsg())
-}
-
-func (b *badProxyTransferInfo) generateBadNodeMsg() (msg string) {
-	if b.nodeDown {
-		msg = "\n" + fmt.Sprintf(badNodeMsgf, b.node)
-	}
-	return
-}
-
-type copyPassChmodFailProxyTransferInfo struct {
-	account           string
-	role              string
-	node              string
-	nodeDown          bool
-	proxyFileName     string
-	proxyFilePathSrc  string
-	proxyFileNameDest string
-}
-
-func (b *copyPassChmodFailProxyTransferInfo) createCopyProxiesStatus() copyProxiesStatus {
-	return copyProxiesStatus{b.node, b.account, b.role, nil}
-}
-
-func (b *copyPassChmodFailProxyTransferInfo) copyProxy(ctx context.Context, s []string) error {
-	time.Sleep(1 * time.Second)
-	if e := ctx.Err(); e != nil {
-		return e
-	}
-	return nil
-}
-
-func (b *copyPassChmodFailProxyTransferInfo) chmodProxy(ctx context.Context, s []string) error {
-	time.Sleep(1 * time.Second)
-	// Purposefully not checking context here
-	return fmt.Errorf("Error changing permission of proxy %s to mode 400 on %s."+
-		"The error was test error.  %s", b.proxyFileName, b.node, b.generateBadNodeMsg())
-}
-
-func (b *copyPassChmodFailProxyTransferInfo) generateBadNodeMsg() (msg string) {
-	if b.nodeDown {
-		msg = "\n" + fmt.Sprintf(badNodeMsgf, b.node)
-	}
-	return
-}
-
-func TestCopyProxies(t *testing.T) {
+func TestCopyProxiesCount(t *testing.T) {
 	var i, j int
-	numGood := 2
-	numBad := 1
+	var testPointers []pushProxyer
+	numGood := 1
+	numBad := 2
 	ctx := context.Background()
-	var g1, g2 goodProxyTransferInfo
-	var b badProxyTransferInfo
-	var gb copyPassChmodFailProxyTransferInfo
 
+	// Test cases
+	var countTests = []struct {
+		f               fakeProxyTransferInfo
+		expectedMsgPart string
+	}{
+		{
+			f: fakeProxyTransferInfo{
+				tag:               "good transfer",
+				copyErr:           nil,
+				chmodErr:          nil,
+				contextCopyCheck:  false,
+				proxyTransferInfo: proxyTransferInfo{},
+			},
+			expectedMsgPart: "",
+		},
+		{
+			f: fakeProxyTransferInfo{
+				tag:               "bad transfer",
+				copyErr:           errors.New("Copying proxy to node failed.  The error was test error"),
+				chmodErr:          errors.New("Error changing permission of proxy to mode 400. The error was test error"),
+				contextCopyCheck:  false,
+				proxyTransferInfo: proxyTransferInfo{},
+			},
+			expectedMsgPart: "Copying proxy",
+		},
+		{
+			f: fakeProxyTransferInfo{
+				tag:               "bad transfer",
+				copyErr:           errors.New("Copying proxy to node failed.  The error was test error"),
+				chmodErr:          errors.New("Error changing permission of proxy to mode 400. The error was test error"),
+				contextCopyCheck:  false,
+				proxyTransferInfo: proxyTransferInfo{},
+			},
+			expectedMsgPart: "Error changing permission of proxy",
+		},
+	}
+
+	// Tests
 	if testing.Verbose() {
 		t.Logf("Testing mocking pushing voms proxies - %d successful, %d bad.", numGood, numBad)
 	}
-	copyChannel := copyProxies(ctx, &g1, &b, &g2)
+
+	for idx := range countTests {
+		testPointers = append(testPointers, &countTests[idx].f)
+	}
+
+	copyChannel := copyProxies(ctx, testPointers...)
+
 	for c := range copyChannel {
 		if c.err != nil {
+			fmt.Println(c.err)
 			j++
 		} else {
 			i++
@@ -135,72 +81,147 @@ func TestCopyProxies(t *testing.T) {
 	if i != numGood || j != numBad {
 		t.Errorf("Expected %d good, %d bad proxies.  Got %d good, %d bad instead.", numGood, numBad, i, j)
 	}
+}
 
-	if testing.Verbose() {
-		t.Log("Testing mocking copy fails")
-	}
-	expectedMsgPart := "Copying proxy"
-	copyChannel = copyProxies(ctx, &b)
-	c := <-copyChannel
-	if !strings.Contains(c.err.Error(), expectedMsgPart) {
-		t.Errorf("Expected copy to fail, and thus error should have contained the phrase %s.  Got %s instead", expectedMsgPart, c.err)
+func TestCopyProxiesBasic(t *testing.T) {
+	ctx := context.Background()
+
+	// Test cases
+	var basicTests = []struct {
+		f               fakeProxyTransferInfo
+		expectedMsgPart string
+	}{
+		{
+			f: fakeProxyTransferInfo{
+				tag:               "copy fails",
+				copyErr:           errors.New("Copying proxy to node failed.  The error was test error"),
+				chmodErr:          nil,
+				contextCopyCheck:  false,
+				proxyTransferInfo: proxyTransferInfo{},
+			},
+			expectedMsgPart: "Copying proxy",
+		},
+		{
+			f: fakeProxyTransferInfo{
+				tag:               "copy succeeds, chmod fails",
+				copyErr:           nil,
+				chmodErr:          errors.New("Error changing permission of proxy to mode 400. The error was test error"),
+				contextCopyCheck:  false,
+				proxyTransferInfo: proxyTransferInfo{},
+			},
+			expectedMsgPart: "Error changing permission of proxy",
+		},
 	}
 
-	if testing.Verbose() {
-		t.Log("Testing mocking copy succeeds but chmod fails")
+	// Test loop
+	for _, test := range basicTests {
+		if testing.Verbose() {
+			t.Log(test.f.tag)
+		}
+		copyChannel := copyProxies(ctx, &test.f)
+		c := <-copyChannel
+		if !strings.Contains(c.err.Error(), test.expectedMsgPart) {
+			t.Errorf("Expected state \"%s\", and thus error should have contained the phrase %s.  Got %s instead", test.f.tag, test.expectedMsgPart, c.err)
+		}
 	}
-	expectedMsgPart = "Error changing permission of proxy"
-	copyChannel = copyProxies(ctx, &gb)
-	c = <-copyChannel
-	if !strings.Contains(c.err.Error(), expectedMsgPart) {
-		t.Errorf("Expected chmod to fail, and thus error should have contained the phrase %s.  Got %s instead", expectedMsgPart, c.err)
-	}
+}
+
+func TestBadNode(t *testing.T) {
+	var f fakeProxyTransferInfo
+	ctx := context.Background()
 
 	if testing.Verbose() {
 		t.Log("Testing mocking copy fails but bad node")
 	}
-	b.nodeDown = true
+	f.copyErr = errors.New("Copying proxy to node failed.  The error was test error")
+	f.nodeDown = true
 	expectedMsgPart1 := "Copying proxy"
-	expectedMsgPart2 := fmt.Sprintf(badNodeMsgf, gb.node)
-	copyChannel = copyProxies(ctx, &b)
-	c = <-copyChannel
+	expectedMsgPart2 := fmt.Sprintf(badNodeMsgf, f.node)
+	copyChannel := copyProxies(ctx, &f)
+	c := <-copyChannel
 	if !(strings.Contains(c.err.Error(), expectedMsgPart1) && strings.Contains(c.err.Error(), expectedMsgPart2)) {
 		t.Errorf("Expected chmod to fail, and thus error should have contained the phrases %s and %s.  Got %s instead", expectedMsgPart1, expectedMsgPart2, c.err)
 	}
-	b.nodeDown = false
+}
 
-	if testing.Verbose() {
-		t.Log("Testing mocking copy proxy - timeout")
+func TestTimeouts(t *testing.T) {
+	ctx := context.Background()
+
+	// Test cases
+	var timeoutTests = []struct {
+		f           fakeProxyTransferInfo
+		expectedMsg string
+	}{
+		{
+			f: fakeProxyTransferInfo{
+				tag:               "copy timeout",
+				copyErr:           errors.New("Copying proxy to node failed.  The error was test error"),
+				chmodErr:          nil,
+				contextCopyCheck:  true,
+				proxyTransferInfo: proxyTransferInfo{},
+			},
+			expectedMsg: "context deadline exceeded",
+		},
+		{
+			f: fakeProxyTransferInfo{
+				tag:               "copy succeeds, chmod times out",
+				copyErr:           nil,
+				chmodErr:          errors.New("Error changing permission of proxy to mode 400. The error was test error"),
+				contextCopyCheck:  false,
+				proxyTransferInfo: proxyTransferInfo{},
+			},
+			expectedMsg: "context deadline exceeded",
+		},
 	}
-	timeoutCtx, cancelTimeout := context.WithTimeout(ctx, time.Duration(1*time.Nanosecond))
-	copyChannel = copyProxies(timeoutCtx, &b)
-	c = <-copyChannel
-	if c.err != nil {
-		lowerErr := strings.ToLower(c.err.Error())
-		expectedMsg := "context deadline exceeded"
-		if lowerErr != expectedMsg {
-			t.Errorf("Expected error message to be %s.  Got %s instead", expectedMsg, lowerErr)
+
+	// Test loop
+	for _, test := range timeoutTests {
+		if testing.Verbose() {
+			t.Log(test.f.tag)
 		}
-	} else {
-		t.Error("Expected some timeout error.  Didn't get any")
-	}
-	cancelTimeout()
 
-	if testing.Verbose() {
-		t.Log("Testing mocking copy proxy - copy works, but chmod times out")
-	}
-	timeoutCtx, cancelTimeout = context.WithTimeout(ctx, time.Duration(1*time.Nanosecond))
-	copyChannel = copyProxies(timeoutCtx, &gb)
-	c = <-copyChannel
-	if c.err != nil {
-		lowerErr := strings.ToLower(c.err.Error())
-		expectedMsg := "context deadline exceeded"
-		if lowerErr != expectedMsg {
-			t.Errorf("Expected error message to be %s.  Got %s instead", expectedMsg, lowerErr)
+		timeoutCtx, cancelTimeout := context.WithTimeout(ctx, time.Duration(1*time.Nanosecond))
+		copyChannel := copyProxies(timeoutCtx, &test.f)
+		c := <-copyChannel
+		if c.err != nil {
+			lowerErr := strings.ToLower(c.err.Error())
+			if lowerErr != test.expectedMsg {
+				t.Errorf("Expected error message to be %s.  Got %s instead", test.expectedMsg, lowerErr)
+			}
+		} else {
+			t.Error("Expected some timeout error.  Didn't get any error")
 		}
-	} else {
-		t.Error("Expected some timeout error.  Didn't get any")
+		cancelTimeout()
 	}
-	cancelTimeout()
+}
 
+type fakeProxyTransferInfo struct {
+	tag              string
+	copyErr          error
+	chmodErr         error
+	contextCopyCheck bool
+	proxyTransferInfo
+}
+
+func (f *fakeProxyTransferInfo) createCopyProxiesStatus() copyProxiesStatus {
+	return copyProxiesStatus{f.node, f.account, f.role, nil}
+}
+
+func (f *fakeProxyTransferInfo) copyProxy(ctx context.Context, opts []string) error {
+	time.Sleep(1 * time.Second)
+	if f.contextCopyCheck {
+		if e := ctx.Err(); e != nil {
+			return e
+		}
+	}
+	return f.copyErr
+}
+
+func (f *fakeProxyTransferInfo) chmodProxy(ctx context.Context, opts []string) error {
+	time.Sleep(1 * time.Second)
+	// We want to always check context here
+	if e := ctx.Err(); e != nil {
+		return e
+	}
+	return f.chmodErr
 }
