24cdf060a42c0f600b08abc54dc9f4290ea37d00
sbhat <sbhat@fnal.gov>
2018-12-19 17:08:14 -0600

    Added logging to vomsProxy

--- proxy/vomsProxy.go
+++ proxy/vomsProxy.go
@@ -10,6 +10,8 @@ import (
 	"strings"
 	"text/template"
 
+	log "github.com/sirupsen/logrus"
+
 	"cdcvs.fnal.gov/discompsupp/ken_proxy_push/utils"
 )
 
@@ -48,21 +50,45 @@ type VomsProxy struct {
 func NewVomsProxy(ctx context.Context, vp VomsProxyer, vomsFQAN string) (*VomsProxy, error) {
 	v, err := vp.getVomsProxy(ctx, vomsFQAN)
 	if err != nil {
-		return &VomsProxy{}, err
+		err := "Could not generate a VOMS proxy"
+		log.WithFields(log.Fields{
+			"vomsProxyer": fmt.Sprintf("%v", vp),
+			"FQAN":        vomsFQAN,
+		}).Error(err)
+		return &VomsProxy{}, errors.New(err)
 	}
+	log.WithFields(log.Fields{
+		"Path": v.Path,
+		"FQAN": v.FQAN,
+		"DN":   v.DN,
+	}).Debug("Generated VOMS Proxy successfully")
 	return v, nil
 }
 
 func (v *VomsProxy) Remove() error {
 	if err := os.Remove(v.Path); os.IsNotExist(err) {
-		return errors.New("VOMS Proxy file does not exist")
-	} else {
+		err := "VOMS Proxy file does not exist"
+		log.WithField("path", v.Path).Error(err)
+		return errors.New(err)
+	} else if err != nil {
+		log.WithField("path", v.Path).Error(err)
 		return err
 	}
+	log.WithField("path", v.Path).Debug("VOMS Proxy removed")
+	return nil
 }
 
 func (v *VomsProxy) CopyProxy(ctx context.Context, node, account, dest string) error {
-	return rsyncFile(ctx, v.Path, node, account, dest, sshOpts)
+	err := rsyncFile(ctx, v.Path, node, account, dest, sshOpts)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"sourcePath": v.Path,
+			"destPath":   dest,
+			"node":       node,
+			"account":    account,
+		}).Error("Could not copy VOMS proxy to destination node")
+	}
+	return err
 }
 
 func (s *serviceCert) getVomsProxy(ctx context.Context, vomsFQAN string) (*VomsProxy, error) {
@@ -70,7 +96,9 @@ func (s *serviceCert) getVomsProxy(ctx context.Context, vomsFQAN string) (*VomsP
 
 	_outfile, err := ioutil.TempFile("", "managed_proxy_voms_")
 	if err != nil {
-		return &VomsProxy{}, fmt.Errorf("Couldn't get tempfile, %s", err.Error())
+		err := fmt.Sprintf("Couldn't get tempfile: %s", err.Error())
+		log.WithField("certPath", s.getCertPath()).Error(err)
+		return &VomsProxy{}, errors.New(err)
 	}
 	outfile := _outfile.Name()
 
@@ -82,27 +110,48 @@ func (s *serviceCert) getVomsProxy(ctx context.Context, vomsFQAN string) (*VomsP
 	}
 
 	if err := vpiTemplate.Execute(&b, cArgs); err != nil {
-		return &VomsProxy{}, fmt.Errorf("Could not execute voms-proxy-init template: %s", err.Error())
+		err := fmt.Sprintf("Could not execute voms-proxy-init template: %s", err.Error())
+		log.WithField("certPath", s.getCertPath()).Error(err)
+		return &VomsProxy{}, errors.New(err)
 	}
 
 	args, err := utils.GetArgsFromTemplate(b.String())
 	if err != nil {
-		return &VomsProxy{}, fmt.Errorf("Could not get voms-proxy-init command arguments from template: %s", err.Error())
+		err := fmt.Sprintf("Could not get voms-proxy-init command arguments from template: %s", err.Error())
+		log.WithField("certPath", s.getCertPath()).Error(err)
+		return &VomsProxy{}, errors.New(err)
 	}
 
 	cmd := exec.CommandContext(ctx, vomsProxyExecutables["voms-proxy-init"], args...)
 	if err := cmd.Run(); err != nil {
-		return &VomsProxy{}, fmt.Errorf("Could not execute voms-proxy-init command: %s", err.Error())
+		err := fmt.Sprintf("Could not execute voms-proxy-init command: %s", err.Error())
+		log.WithFields(log.Fields{
+			"certPath": s.getCertPath(),
+			"vomsFQAN": vomsFQAN,
+			"command":  strings.Join(cmd.Args, " "),
+		}).Error(err)
+		return &VomsProxy{}, errors.New(err)
 	}
 
 	v := VomsProxy{Path: outfile, FQAN: vomsFQAN}
 
 	_dn, err := s.getCertSubject(ctx)
 	if err != nil {
-		return &VomsProxy{}, fmt.Errorf("Could not get proxy subject from voms proxy: %s", err.Error())
+		err := "Could not get proxy subject from voms proxy"
+		log.WithFields(log.Fields{
+			"certPath": s.getCertPath(),
+			"vomsFQAN": vomsFQAN,
+		}).Error(err)
+		return &VomsProxy{}, errors.New(err)
 	}
 	v.DN = _dn
 	v.Cert = s
+	log.WithFields(log.Fields{
+		"path":     v.Path,
+		"subject":  v.DN,
+		"vomsFQAN": v.FQAN,
+	}).Debug("Successfully generated VOMS proxy")
+
 	return &v, nil
 }
 
@@ -119,18 +168,38 @@ func rsyncFile(ctx context.Context, source, node, account, dest string, sshOptio
 	}
 
 	if err := rsyncTemplate.Execute(&b, cArgs); err != nil {
-		return fmt.Errorf("Could not execute rsyncTemplate: %s", err.Error())
+		err := fmt.Sprintf("Could not execute rsync template: %s", err.Error())
+		log.WithField("source", source).Error(err)
+		return errors.New(err)
 	}
 
 	args, err := utils.GetArgsFromTemplate(b.String())
 	if err != nil {
-		return fmt.Errorf("Could not get rsync command arguments from template: %s", err.Error())
+		err := fmt.Sprintf("Could not get rsync command arguments from template: %s", err.Error())
+		log.WithField("source", source).Error(err)
+		return errors.New(err)
 	}
 
 	cmd := exec.CommandContext(ctx, vomsProxyExecutables["rsync"], args...)
-	if out, err := cmd.CombinedOutput(); err != nil {
-		return errors.New(fmt.Sprintf("%s\n%s", string(out), string(err.Error())))
+	if err := cmd.Run(); err != nil {
+		err := fmt.Sprintf("rsync command failed: %s", err.Error())
+		log.WithFields(log.Fields{
+			"sshOpts":    sshOptions,
+			"sourcePath": source,
+			"account":    account,
+			"node":       node,
+			"destPath":   dest,
+			"command":    strings.Join(cmd.Args, " "),
+		}).Error(err)
+
+		return errors.New(err)
 	}
+
+	log.WithFields(log.Fields{
+		"account":  account,
+		"node":     node,
+		"destPath": dest,
+	}).Debug("rsync successful")
 	return nil
 
 }
@@ -141,7 +210,9 @@ func (v *VomsProxy) getKeyPath() string  { return v.Cert.getKeyPath() }
 func (v *VomsProxy) getCertSubject(ctx context.Context) (string, error) {
 	dn, err := v.Cert.getCertSubject(ctx)
 	if err != nil {
-		return "", err
+		err := "Could not get subject for VOMS proxy"
+		log.WithField("certPath", v.Cert.getCertPath()).Error(err)
+		return "", errors.New(err)
 	}
 	return dn, nil
 }
@@ -151,6 +222,6 @@ func (v *VomsProxy) getCertSubject(ctx context.Context) (string, error) {
 func init() {
 
 	if err := utils.CheckForExecutables(vomsProxyExecutables); err != nil {
-		fmt.Printf("Note that one or more required executables were not found in $PATH: %s\n", err)
+		log.WithField("executableGroup", "vomsProxy").Error("One or more required executables were not found in $PATH.  Will still attempt to run, but this will probably fail")
 	}
 }
