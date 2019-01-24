f5604c7ab89ac86b7592536b381a1eba815bdbde
sbhat <sbhat@fnal.gov>
2019-01-24 16:30:28 -0600

    Added a mainUtils file; made some minor stylistic changes

---
+++ cmd/store-in-myproxy/mainUtils.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os/user"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+
+	"cdcvs.fnal.gov/discompsupp/ken_proxy_push/experiment"
+	"cdcvs.fnal.gov/discompsupp/ken_proxy_push/notifications"
+)
+
+// createExptConfig takes the config information from the global file and creates an exptConfig object
+func createExptConfig(expt string) (experiment.ExptConfig, error) {
+	var vomsprefix, certfile, keyfile string
+	var c experiment.ExptConfig
+
+	exptKey := "experiments." + expt
+	if !viper.IsSet(exptKey) {
+		err := errors.New("Experiment is not configured in the configuration file")
+		log.WithFields(log.Fields{
+			"experiment": expt,
+		}).Error(err)
+		return c, err
+	}
+
+	exptSubConfig := viper.Sub(exptKey)
+
+	if exptSubConfig.IsSet("vomsgroup") {
+		vomsprefix = exptSubConfig.GetString("vomsgroup")
+	} else {
+		vomsprefix = viper.GetString("vomsproxyinit.defaultvomsprefixroot") + expt + "/"
+	}
+
+	if exptSubConfig.IsSet("certfile") {
+		certfile = exptSubConfig.GetString("certfile")
+	}
+	if exptSubConfig.IsSet("keyfile") {
+		keyfile = exptSubConfig.GetString("keyfile")
+	}
+
+	c = experiment.ExptConfig{
+		Name:           expt,
+		CertBaseDir:    viper.GetString("global.cert_base_dir"),
+		DestDir:        exptSubConfig.GetString("dir"),
+		Nodes:          exptSubConfig.GetStringSlice("nodes"),
+		Accounts:       exptSubConfig.GetStringMapString("accounts"),
+		VomsPrefix:     vomsprefix,
+		CertFile:       certfile,
+		KeyFile:        keyfile,
+		TimeoutsConfig: tConfig,
+		KerbConfig:     krbConfig,
+	}
+
+	log.WithField("experiment", c.Name).Debug("Set up experiment config")
+	return c, nil
+
+}
+
+// setAdminEmail sets the notifications config objects' From and To fields to the config file's admin value
+func setAdminEmail(pnConfig *notifications.Config) {
+	pnConfig.From = pnConfig.ConfigInfo["admin_email"]
+	pnConfig.To = []string{pnConfig.ConfigInfo["admin_email"]}
+	log.Debug("Set notifications config email values to admin defaults")
+	return
+}
+
+// checkUser makes sure that the user running the executable is the authorized user.
+func checkUser(authuser string) error {
+	cuser, err := user.Current()
+	if err != nil {
+		return errors.New("Could not lookup current user.  Exiting")
+	}
+	log.Debug("Running script as ", cuser.Username)
+	if cuser.Username != authuser {
+		return fmt.Errorf("This must be run as %s.  Trying to run as %s", authuser, cuser.Username)
+	}
+	return nil
+}
