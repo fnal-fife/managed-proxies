f61d2cb085f69c4a998b1cc5072041d31b389762
sbhat <sbhat@fnal.gov>
2019-01-30 11:12:38 -0600

    Added unit test for jobsubServerUtils

---
+++ jobsubServerUtils/jobsubServerUtils_test.go
@@ -0,0 +1,38 @@
+package jobsubServerUtils
+
+import (
+	"errors"
+	"testing"
+)
+
+func TestCheckRetrievers(t *testing.T) {
+	tests := []struct {
+		retrievers, defaultRetrievers string
+		expectedError                 error
+	}{
+		{
+			retrievers:        "foo",
+			defaultRetrievers: "foo",
+			expectedError:     nil,
+		},
+		{
+			retrievers:        "foo",
+			defaultRetrievers: "bar",
+			expectedError:     errors.New("Attention:  The retrievers on the jobsub server do not match the default retrievers"),
+		},
+	}
+
+	for _, test := range tests {
+		if err := CheckRetrievers(test.retrievers, test.defaultRetrievers); errorString(err) != errorString(test.expectedError) {
+			t.Errorf("Expected errors didn't match:  wanted %s, got %s", test.expectedError, err)
+		}
+	}
+}
+
+// Allow us to check values of nil errors against each other
+func errorString(err error) string {
+	if err == nil {
+		return ""
+	}
+	return err.Error()
+}
