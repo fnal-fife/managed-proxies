62f4f95880e60f35661802d7ff160f0119cf7809
sbhat <sbhat@fnal.gov>
2019-08-14 13:50:53 -0500

    Added comment

--- packaging/packaging.go
+++ packaging/packaging.go
@@ -3,4 +3,5 @@ package packaging
 // Version of Managed Proxies library
 const Version = "2.4"
 
+// Build version of library.  Intended to be filled in via ldflags sent to go build
 var Build = "."
