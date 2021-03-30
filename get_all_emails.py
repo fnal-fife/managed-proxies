214e68e56a8453b3bf48159202b02a4342a2278c
sbhat <sbhat@fnal.gov>
2021-03-30 10:09:15 -0500

    Added experiment flag to get_all_emails utility

--- get_all_emails.py
+++ get_all_emails.py
@@ -7,7 +7,15 @@ parser = ArgumentParser()
 
 parser.add_argument('-i', '--infile', type=str, default=INFILE)
 parser.add_argument('-d', '--delimiter', type=str, default=', ')
+parser.add_argument('-e', '--experiment', nargs="*")
 args = parser.parse_args()
+
+
+def filter_experiments(expt):
+    if args.experiment is None:
+        return True
+    else:
+        return expt in args.experiment
 
 with open(args.infile, 'r') as f:
     data = yaml.load(f.read(), Loader=yaml.BaseLoader)
@@ -15,7 +23,9 @@ with open(args.infile, 'r') as f:
 s = set()
 
 email_lists = (expt_config['emails']
-               for expt_config in data['experiments'].values())
+               for expt_name, expt_config in data['experiments'].items()
+               if filter_experiments(expt_name))
+
 
 for email_list in email_lists:
     for email in email_list:
